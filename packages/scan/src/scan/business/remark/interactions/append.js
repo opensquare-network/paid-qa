03ff41ce533c36510d267150ee9ddee8028e1c1a
Yongfeng LI <wliyongfeng@gmail.com>
2022-02-27 23:27:38 +0800

    Insert appendix

--- packages/scan/src/scan/business/remark/interactions/append.js
+++ packages/scan/src/scan/business/remark/interactions/append.js
@@ -1,5 +1,15 @@
-async function handleAppend(interaction, caller, indexer) {
+const { insertAppendix } = require("../../../../mongo/service/appendix");
 
+async function handleAppend(interaction, caller, indexer) {
+  const appendix = {
+    caller,
+    topicIpfsCid: interaction.topicIpfsCid,
+    messageIpfsCid: interaction.messageIpfsCid,
+    parsed: false,
+    indexer: indexer.toJSON(),
+  }
+
+  await insertAppendix(appendix);
 }
 
 module.exports = {
