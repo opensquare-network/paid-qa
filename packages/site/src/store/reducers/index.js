d91a0374fbf6fb972487033e2d1d10e2bf940283
yoshiyuki <brother-zhou@qq.com>
2022-03-03 11:29:40 +0800

    store: clean redux test codes & import real slices (#24)

--- packages/site/src/store/reducers/index.js
+++ packages/site/src/store/reducers/index.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-import testReducer from "./testSlice";
+import showConnectReducer from "./showConnectSlice";
 
 export default combineReducers({
-  test: testReducer,
+  showConnect: showConnectReducer,
 });
