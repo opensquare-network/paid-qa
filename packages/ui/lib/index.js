a79a2f0f790b86ec0fd7c22e2fa7caefd408e694
yoshiyuki <brother-zhou@qq.com>
2022-03-03 16:47:31 +0800

    ui: import semantic & add patch for it under webpack5

--- packages/ui/lib/index.js
+++ packages/ui/lib/index.js
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import ExternalLink from "./ExternalLink";
 import ChainIcon from "./Chain/ChainIcon";
+import Dropdown from "./styled/Dropdown";
 
-export { Footer, ExternalLink, ChainIcon };
+export { Footer, ExternalLink, ChainIcon, Dropdown };
