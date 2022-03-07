671ad9d5d612e1463470d6896836119f1428f52e
yoshiyuki <brother-zhou@qq.com>
2022-03-07 12:41:57 +0800

    fix: invalid async/await syntax

--- packages/site/src/services/ipfs.js
+++ packages/site/src/services/ipfs.js
@@ -1,7 +1,6 @@
 import Hash from "ipfs-only-hash";
 
-export function cidOf(obj) {
+export async function cidOf(obj) {
   const data = JSON.stringify(obj);
-  const cid = await Hash.of(Buffer.from(data));
-  return cid;
+  return await Hash.of(Buffer.from(data));
 }
