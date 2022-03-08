399e9649b1e7fa37e14fcefa57de98ec0e218034
Yongfeng LI <wliyongfeng@gmail.com>
2022-03-08 16:03:04 +0800

    Go to home page after clicking header logo (#54)

--- packages/site/src/components/Header.js
+++ packages/site/src/components/Header.js
@@ -14,6 +14,7 @@ import {
   popUpConnect,
   showConnectSelector,
 } from "../store/reducers/showConnectSlice";
+import { NavLink } from "react-router-dom";
 
 const Wrapper = styled.header`
   position: relative;
@@ -104,7 +105,9 @@ export default function Header() {
     <Wrapper>
       <ContentWrapper>
         <LeftWrapper>
-          <Logo />
+          <NavLink to="/">
+            <Logo />
+          </NavLink>
           <Divider />
           <AppWrapper>
             <img src="/imgs/icons/apps.svg" alt="" />
