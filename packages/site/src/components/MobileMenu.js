f442194aa94d50f8c358f5bca4bdcd6f6dda92e3
yoshiyuki <brother-zhou@qq.com>
2022-03-09 11:36:04 +0800

    improve <Header> responsive, #59 (#66)
    * improve <Header> responsive, #59
    
    * fix no background issue

--- packages/site/src/components/MobileMenu.js
+++ packages/site/src/components/MobileMenu.js
@@ -1,23 +1,41 @@
 import styled from "styled-components";
 import { useState } from "react";
-import { p_14_medium } from "../styles/textStyles";
+import { p_12_normal, p_14_medium } from "../styles/textStyles";
 
 const Wrapper = styled.div``;
 
-const FloatingWrapper = styled.div`
+const FloatingWrapper = styled.ul`
   position: absolute;
   left: 0;
   right: 0;
   top: 60px;
   z-index: 1;
-  padding: 20px;
+  margin: 0;
+  padding: 4px 20px;
+  background: white;
+  li {
+    padding-top: 16px;
+    padding-bottom: 16px;
+    ::before {
+      content: "";
+      background-image: url("/imgs/icons/caret-right.svg");
+      float: right;
+      margin-top: 9px;
+      height: 24px;
+      width: 24px;
+    }
+  }
 
   * {
     margin: 0;
     display: block;
     ${p_14_medium};
-    line-height: 40px;
     color: #1e2134;
+  }
+
+  span {
+    ${p_12_normal};
+    color: #a1a8b3;
   }
 
   > :not(:last-child) {
@@ -36,8 +54,18 @@ function MobileMenu() {
       <img src={`/imgs/icons/${showMenu ? "close" : "menu"}.svg`} alt="" />
       {showMenu && (
         <FloatingWrapper>
-          <a href="https://voting.opensquare.io/">Off-chain Voting</a>
-          <p>Paid QA</p>
+          <li>
+            <a href="https://voting.opensquare.io/">
+              <p>Off-chain Voting</p>
+              <span>Voting system for Polkadot ecosystem</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <p>Paid QA</p>
+              <span>Decentralized QA Platform</span>
+            </a>
+          </li>
         </FloatingWrapper>
       )}
     </Wrapper>
