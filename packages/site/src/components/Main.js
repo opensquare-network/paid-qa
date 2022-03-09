022c0e6a3e9728a5a004bddbaec4e4e30ffcb1bc
yoshiyuki <brother-zhou@qq.com>
2022-03-09 14:37:08 +0800

    set responsive css

--- packages/site/src/components/Main.js
+++ packages/site/src/components/Main.js
@@ -2,6 +2,12 @@ import styled from "styled-components";
 
 const Main = styled.main`
   flex: 1 1 auto;
+  @media screen and (min-width: 1144px) {
+    padding: 0 32px;
+  }
+  @media screen and (min-width: 768px) {
+    padding: 0 16px;
+  }
 `;
 
 export default Main;
