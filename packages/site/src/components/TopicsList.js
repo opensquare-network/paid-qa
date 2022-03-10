0ea0b925f63328548b0c08f8fcdc43c24e394aa2
yoshiyuki <brother-zhou@qq.com>
2022-03-10 12:41:33 +0800

    improve skeleton style

--- packages/site/src/components/TopicsList.js
+++ packages/site/src/components/TopicsList.js
@@ -4,6 +4,7 @@ import Topic from "components/Topic";
 
 const Wrapper = styled.div`
   svg {
+    background: white;
     box-shadow: 0px 4px 31px rgba(26, 33, 44, 0.04),
       0px 0.751293px 3.88168px rgba(26, 33, 44, 0.03);
   }
@@ -11,17 +12,19 @@ const Wrapper = styled.div`
 
 const MyLoader = (props) => (
   <ContentLoader
-    speed={2}
+    speed={1}
     width="100%"
-    height={131}
-    viewBox="0 0 1080 131"
+    height={153}
+    viewBox="0 0 1080 153"
     backgroundColor="#f3f3f3"
-    foregroundColor="#ecebeb"
+    foregroundColor="#EFF3F9"
     {...props}
   >
-    <rect x="180" y="24" rx="3" ry="3" width="100%" height="6" />
-    <rect x="180" y="88" rx="3" ry="3" width="178" height="6" />
-    <rect x="20" y="20" rx="3" ry="3" width="80" height="80" />
+    <rect x="24" y="28" rx="3" ry="3" width="1032" height="16" />
+    <rect x="24" y="52" rx="3" ry="3" width="178" height="16" />
+    <rect x="24" y="88" rx="3" ry="3" width="1032" height="1" />
+    <circle cx="36" cy="118" r="12" />
+    <rect x="56" y="109" rx="3" ry="3" width="128" height="16" />
   </ContentLoader>
 );
 
