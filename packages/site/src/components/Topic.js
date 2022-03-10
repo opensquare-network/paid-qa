f42bf5ebc6b9b2605265789cd2511f856abb4b67
yoshiyuki <brother-zhou@qq.com>
2022-03-10 16:51:02 +0800

    fix topic list title color (#91)
    * fix topic list title color
    
    * fix topic list title color

--- packages/site/src/components/Topic.js
+++ packages/site/src/components/Topic.js
@@ -14,6 +14,7 @@ const Wrapper = styled.div`
   border: 1px solid #f0f3f8;
   box-shadow: 0px 4px 31px rgba(26, 33, 44, 0.04),
     0px 0.751293px 3.88168px rgba(26, 33, 44, 0.03);
+  color: #1e2134;
 `;
 
 const ReplyWrapper = styled.div`
