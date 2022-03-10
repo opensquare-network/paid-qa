290ac103b89e07484e014756d18392e54d00b9a0
yoshiyuki <brother-zhou@qq.com>
2022-03-10 14:54:17 +0800

    Skeleton support responsive (#85)

--- packages/site/src/components/TopicsList.js
+++ packages/site/src/components/TopicsList.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import ContentLoader from "react-content-loader";
 import Topic from "components/Topic";
+import { useWindowSize } from "ui/lib/utils/hooks";
 
 const Wrapper = styled.div`
   svg {
@@ -10,28 +11,34 @@ const Wrapper = styled.div`
   }
 `;
 
-const MyLoader = (props) => (
-  <ContentLoader
-    speed={1}
-    width="100%"
-    height={153}
-    viewBox="0 0 1080 153"
-    backgroundColor="#f3f3f3"
-    foregroundColor="#EFF3F9"
-    {...props}
-  >
-    <rect x="24" y="28" rx="3" ry="3" width="1032" height="16" />
-    <rect x="24" y="52" rx="3" ry="3" width="178" height="16" />
-    <rect x="24" y="88" rx="3" ry="3" width="1032" height="1" />
-    <circle cx="36" cy="118" r="12" />
-    <rect x="56" y="109" rx="3" ry="3" width="128" height="16" />
-  </ContentLoader>
-);
+const MyLoader = (props) => {
+  const windowSize = useWindowSize();
+  return (
+    <ContentLoader
+      speed={1}
+      width="100%"
+      height={153}
+      viewBox={`0 0 ${
+        windowSize.width > 1144 ? 1080 : windowSize.width - 64
+      } 153`}
+      backgroundColor="#f3f3f3"
+      foregroundColor="#EFF3F9"
+      {...props}
+    >
+      <rect x="24" y="28" rx="3" ry="3" width="1032" height="16" />
+      <rect x="24" y="52" rx="3" ry="3" width="178" height="16" />
+      <rect x="24" y="88" rx="3" ry="3" width="1032" height="1" />
+      <circle cx="36" cy="118" r="12" />
+      <rect x="56" y="109" rx="3" ry="3" width="128" height="16" />
+    </ContentLoader>
+  );
+};
 
 export default function TopicsList({ topics }) {
   //TODO: show empty placeholder for no topics
   return (
     <Wrapper>
+      <MyLoader />
       {!topics ? (
         <MyLoader />
       ) : (
