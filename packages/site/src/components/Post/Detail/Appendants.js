85fd7f8c2a008a4dd7ee81ef34f1bf9b244a87f0
hyifeng <chaojun@opensquare.network>
2022-03-11 10:49:16 +0800

    Refs #44, implement add appendant ui (#93)
    * Refs #44, implement add appendant ui
    
    * Update
    
    * Update

--- packages/site/src/components/Post/Detail/Appendants.js
+++ packages/site/src/components/Post/Detail/Appendants.js
@@ -1,6 +1,12 @@
 import styled from "styled-components";
+import { useState } from "react";
+import { ReactComponent as AddIcon } from "./icons/add-appendant.svg";
 
 import DividerWrapper from "ui/lib/styled/DividerWrapper";
+import RichEdit from "ui/lib/RichEdit";
+import Time from "ui/lib/Time";
+import IpfsSquare from "ui/lib/IpfsSquare";
+import FlexBetween from "ui/lib/styled/FlexBetween";
 
 const Wrapper = styled.div`
   > :first-child {
@@ -38,40 +44,71 @@ const StyledDividerWrapper = styled(DividerWrapper)`
   }
 `;
 
-export default function Appendants({ appendants }) {
-  if (!appendants || appendants.length === 0) {
+const Title = styled(FlexBetween)`
+`;
+
+const AddButton = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const EditorWrapper = styled.div``;
+
+const Count = styled.div`
+  color: #a1a8b3;
+`;
+
+export default function Appendants({ appendants, isOwner }) {
+  const [editing, setEditing] = useState(false);
+  const [content, setContent] = useState("");
+  const onSubmit = () => {
+    //TODO: Handle submitting appendant
+  };
+
+  const appendantsCount = appendants?.length || 0;
+  if (!isOwner && !appendantsCount) {
     return null;
   }
 
   return (
     <Wrapper>
-      <div>Appendants</div>
-      <ItemWrapper>
-        <div>
-          <StyledDividerWrapper>
-            <div>#1</div>
-            <div>12 hours ago</div>
-          </StyledDividerWrapper>
-          <img src="/imgs/icons/ipfs.svg" alt="" />
-        </div>
-        <div>
-          Velit amet auctor feugiat consectetur malesuada suspendisse facilisi.
-          Eget fringilla eu semper vivamus morbi nunc arcu pellentesque ac.
-        </div>
-      </ItemWrapper>
-      <ItemWrapper>
-        <div>
-          <StyledDividerWrapper>
-            <div>#2</div>
-            <div>12 hours ago</div>
-          </StyledDividerWrapper>
-          <img src="/imgs/icons/ipfs.svg" alt="" />
-        </div>
-        <div>
-          Velit amet auctor feugiat consectetur malesuada suspendisse facilisi.
-          Eget fringilla eu semper vivamus morbi nunc arcu pellentesque ac.
-        </div>
-      </ItemWrapper>
+      <Title>
+        <DividerWrapper>
+          <div>Appendants</div>
+          <Count>{appendantsCount}</Count>
+        </DividerWrapper>
+        <AddButton onClick={() => setEditing(!editing)}>
+          <AddIcon />
+        </AddButton>
+      </Title>
+      {appendants?.map((item, index) => (
+        <ItemWrapper key={index}>
+          <div>
+            <StyledDividerWrapper>
+              <div>{`#${index + 1}`}</div>
+              <Time time={item.blockTime} />
+            </StyledDividerWrapper>
+            <IpfsSquare
+              href={
+                item.pinned ? `https://ipfs.infura.io/ipfs/${item.cid}` : null
+              }
+            />
+          </div>
+          <div>{item.content}</div>
+        </ItemWrapper>
+      ))}
+      {editing && (
+        <EditorWrapper>
+          <RichEdit
+            content={content}
+            setContent={setContent}
+            onSubmit={onSubmit}
+            showButtons={true}
+          />
+        </EditorWrapper>
+      )}
     </Wrapper>
   );
 }
