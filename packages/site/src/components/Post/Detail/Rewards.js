929c3694dfd6a42bb89bda3540df5c552faa2845
hyifeng <chaojun@opensquare.network>
2022-03-21 18:35:33 +0800

    Refs #138, Add Funders info to topic detail header (#160)

--- packages/site/src/components/Post/Detail/Rewards.js
+++ packages/site/src/components/Post/Detail/Rewards.js
@@ -1,7 +1,17 @@
+import flatten from "lodash.flatten";
+import Avatar from "@osn/common-ui/lib/Account/Avatar";
 import styled from "styled-components";
 import { calcRewards } from "utils/rewards";
+import { useSelector } from "react-redux";
+import { fundSummarySelector } from "store/reducers/topicSlice";
 
 const Wrapper = styled.div`
+  display: flex;
+  align-items: flex-start;
+`;
+
+const Item = styled.div`
+  flex-grow: 1;
   > :first-child {
     font-weight: 600;
     font-size: 16px;
@@ -15,14 +25,46 @@ const Wrapper = styled.div`
   }
 `;
 
-export default function Rewards({ rewards }) {
-  const tokenValues = calcRewards(rewards);
+const FunderAvatarList = styled.div`
+  display: flex;
+`;
+
+export default function Rewards() {
+  const fundSummary = useSelector(fundSummarySelector);
+  const fundsStats = fundSummary?.statsBySponsors;
+
+  if (!fundsStats || Object.keys(fundsStats).length === 0) {
+    return null;
+  }
+
+  const sponsors = Array.from(new Set(Object.keys(fundsStats)));
+  const funds = Object.keys(fundsStats).map((sponsor) =>
+    Object.keys(fundsStats[sponsor]).map((token) => ({
+      sponsor,
+      symbol: token,
+      value: fundsStats[sponsor][token],
+    }))
+  );
+
+  const tokenValues = calcRewards(flatten(funds));
   return (
     <Wrapper>
-      <div>Rewards</div>
-      <div>
-        {Object.keys(tokenValues).map(symbol => `${tokenValues[symbol]} ${symbol}`).join(", ")}
-      </div>
+      <Item>
+        <div>Funders</div>
+        <FunderAvatarList>
+          {sponsors.map((sponsor) => (
+            <Avatar address={sponsor} size={20} />
+          ))}
+        </FunderAvatarList>
+      </Item>
+      <Item>
+        <div>Rewards</div>
+        <div>
+          {Object.keys(tokenValues)
+            .map((symbol) => `${tokenValues[symbol]} ${symbol}`)
+            .join(", ")}
+        </div>
+      </Item>
     </Wrapper>
   );
 }
