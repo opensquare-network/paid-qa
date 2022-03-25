4c7fa6d25e8c4bb52a8b18c02b4b9c532cc36407
chaojun <chaojun@opensquare.network>
2022-03-25 15:21:50 +0800

    Update

--- packages/server/src/models/notification.js
+++ packages/server/src/models/notification.js
@@ -4,7 +4,7 @@ const { Schema } = require("mongoose");
 const NotificationSchema = new mongoose.Schema(
   {
     owner: String, // public key of owner address
-    type: [String], // reply, mention, fund, resolve
+    type: [String], // reply, mention, fund, topicResolved
     read: {
       type: Boolean,
       default: false,
