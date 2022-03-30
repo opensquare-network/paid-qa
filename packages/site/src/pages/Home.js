2d3f438eef33c750d20bf6170d12d5951f08c1aa
yoshiyuki <brother-zhou@qq.com>
2022-03-30 13:44:33 +0800

    style: complete explorer responsive

--- packages/site/src/pages/Home.js
+++ packages/site/src/pages/Home.js
@@ -5,10 +5,14 @@ import Explorer from "components/Explorer";
 import NewTopicAnchor from "components/NewTopicButton";
 import TopicsList from "components/TopicsList";
 import Background from "components/Background";
+import { MOBILE_SIZE } from "@osn/common-ui/lib/utils/constants";
 
 const Wrapper = styled.div`
   position: relative;
   padding: 40px 0 64px;
+  @media screen and (max-width: ${MOBILE_SIZE}px) {
+    padding-top: 20px;
+  }
 `;
 
 const ContentWrapper = styled.div`
