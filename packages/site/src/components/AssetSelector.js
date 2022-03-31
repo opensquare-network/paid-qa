483747c1ce2b799bd194c5ecd3c38265b03958e7
yoshiyuki <brother-zhou@qq.com>
2022-03-31 10:57:09 +0800

    rename .env item

--- packages/site/src/components/AssetSelector.js
+++ packages/site/src/components/AssetSelector.js
@@ -76,7 +76,7 @@ const Assets = [
     symbol: "ARIS",
   },
 ];
-if (process.env.NEXT_PUBLIC_SHOW_WESTEND === "TRUE") {
+if (process.env.REACT_APPSHOW_WESTEND === "TRUE") {
   Assets.push({
     id: "westend",
     name: "Westend",
