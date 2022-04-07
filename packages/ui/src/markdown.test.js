f797fa607d450e0dcc4eea191362b0e1547dee06
yoshiyuki <brother-zhou@qq.com>
2022-04-07 17:09:58 +0800

    test: unit test for ui/lib utils/markdown.js

---
+++ packages/ui/src/markdown.test.js
@@ -0,0 +1,19 @@
+import { matchMdLink, replaceMentionLinks } from "../lib/utils/markdown";
+
+test("matchMdLink", () => {
+  expect(
+    matchMdLink(`##HELLO, 
+  https://www.opensquare.io/ is a great website`)
+  ).toBe(`##HELLO, 
+  [https://www.opensquare.io/](https://www.opensquare.io/) is a great website`);
+});
+
+test("replaceMentionLinks", () => {
+  expect(
+    replaceMentionLinks(
+      `[@DBUi...31H9](/network/westend/address/DBUiVmKrTsv4hetpCRtvmFLgJ54jjwY9QRJqPAQVijP31H9)`
+    )
+  ).toBe(
+    `[@DBUi...31H9](/#/network/westend/address/DBUiVmKrTsv4hetpCRtvmFLgJ54jjwY9QRJqPAQVijP31H9) `
+  );
+});
