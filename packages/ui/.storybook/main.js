7455e8347bb3e6de21982147c38d64d38922f07e
2nthony <ant.hu@outlook.com>
2022-04-15 17:12:36 +0800

    feat(story): pagination (#365)
    * build(story): fix import svg
    
    * chore(deps): babel-plugin-inline-react-svg
    
    * feat(story): pagination

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -6,4 +6,9 @@ module.exports = {
     "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
+  // https://github.com/storybookjs/storybook/issues/6188#issuecomment-822924831
+  babel: (options) => {
+    options.plugins.push("babel-plugin-inline-react-svg");
+    return options;
+  },
 };
