4c1520263e58201a34ad406a9310e816d013335b
2nthony <ant.hu@outlook.com>
2022-04-15 14:14:02 +0800

    feat(ui): nodata story

---
+++ packages/ui/src/stories/NoData.stories.js
@@ -0,0 +1,10 @@
+import NoData from "../../lib/NoData";
+
+export default {
+  title: "NoData",
+  component: NoData,
+};
+
+export const primary = () => <NoData />;
+
+export const withMessagePosts = () => <NoData message="posts" />;
