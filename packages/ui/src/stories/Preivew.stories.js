2f0018589dc077b2aaef569fb18fc9d8326028a6
2nthony <ant.hu@outlook.com>
2022-04-18 17:06:17 +0800

    feat(story): preview (#380)
    * feat(story): preview
    
    * feat(story): preview wysiwyg
    
    * refactor(stpry): preview
    
    prefer e
    
    * refactor(story): prefer event

---
+++ packages/ui/src/stories/Preivew.stories.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { useState } from "react";
+import Preview from "../../lib/Preview";
+
+export default {
+  title: "Preview",
+  component: Preview,
+};
+
+const markdown = `
+# heading 1
+
+## heading 2
+
+### heading 3
+
+**bold text**
+
+_italic text_
+
+- bullet 1
+- bullet 2
+
+1. numbered 1
+2. numbered 2
+`.trim();
+
+const Wrapper = styled.div`
+  display: flex;
+  width: 100%;
+`;
+
+const Textarea = styled.textarea`
+  width: 300px;
+  margin-right: 48px;
+`;
+
+export const primary = () => {
+  const [content, setContent] = useState(markdown);
+
+  return (
+    <Wrapper>
+      <Textarea
+        value={content}
+        onChange={(event) => setContent(event.target.value)}
+      ></Textarea>
+
+      <Preview content={content} />
+    </Wrapper>
+  );
+};
