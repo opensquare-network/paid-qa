139f5695332045d7b537d2d1f4481e0d97cac0be
2nthony <ant.hu@outlook.com>
2022-04-18 21:27:51 +0800

    fix(dropdownselector): import dropdown

--- packages/ui/lib/DropdownSelector/index.js
+++ packages/ui/lib/DropdownSelector/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { Dropdown } from "../index";
+import Dropdown from "../styled/Dropdown";
 
 const Wrapper = styled.div``;
 
