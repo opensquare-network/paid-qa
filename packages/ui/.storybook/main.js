758d62d908482875bcc92a7c53d52dc3f59653c7
2nthony <ant.hu@outlook.com>
2022-04-18 16:13:45 +0800

    fix(storybook): specified the files from `site` and ignore the `storybook-static` (#378)
    * fix(storybook): specified the files from `site`
    
    * chore(gitignore): add storybook-static
    
    * fix(storybook): public files
    
    micromarkmd

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -8,7 +8,23 @@ module.exports = {
   staticDirs: [
     // make `site` as public folder for storybook
     // used in `preview-head.html`
-    "../../site",
+    {
+      from: "../../site/public",
+      to: "/public",
+    },
+    {
+      from: "../../site/src/index.css",
+      to: "/src/index.css",
+    },
+    // used in 'Preview/MicromarkMd'
+    {
+      from: "../../site/public/prism.js",
+      to: "/prism.js",
+    },
+    {
+      from: "../../site/public/prism.css",
+      to: "/prism.css",
+    },
   ],
   framework: "@storybook/react",
   // https://github.com/storybookjs/storybook/issues/6188#issuecomment-822924831
