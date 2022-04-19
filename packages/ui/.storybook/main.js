c6eb698907f8afbaedd008e9928059277e6394fc
2nthony <ant.hu@outlook.com>
2022-04-19 11:28:06 +0800

    feat(storybook): webpack 5 with svg (#390)
    * feat(storybook): webpack 5
    
    * fix(storybook): svg
    
    * chore: lockfile

--- packages/ui/.storybook/main.js
+++ packages/ui/.storybook/main.js
@@ -1,4 +1,7 @@
 module.exports = {
+  core: {
+    builder: "webpack5",
+  },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
@@ -28,8 +31,37 @@ module.exports = {
   ],
   framework: "@storybook/react",
   // https://github.com/storybookjs/storybook/issues/6188#issuecomment-822924831
-  babel: (options) => {
-    options.plugins.push("babel-plugin-inline-react-svg");
-    return options;
+  webpackFinal: (config) => {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    // https://github.com/facebook/create-react-app/blob/f99167c014a728ec856bda14f87181d90b050813/packages/react-scripts/config/webpack.config.js#L388
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            prettier: false,
+            svgo: false,
+            svgoConfig: {
+              plugins: [{ removeViewBox: false }],
+            },
+            titleProp: true,
+            ref: true,
+          },
+        },
+        {
+          loader: "file-loader",
+          options: {
+            name: "static/media/[name].[hash].[ext]",
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
