68b1c18b8fc2a34c88aa94a7395bcfdad4542188
2nthony <ant.hu@outlook.com>
2022-04-20 17:58:42 +0800

    refactor(site): use modal

--- packages/site/src/components/ResolveModal.js
+++ packages/site/src/components/ResolveModal.js
@@ -1,8 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { accountSelector } from "../store/reducers/accountSlice";
 
-import { Modal } from "semantic-ui-react";
-import Button from "@osn/common-ui/lib/styled/Button";
+import Modal from "@osn/common-ui/lib/Modal";
 import styled from "styled-components";
 import {
   p_14_normal,
@@ -30,35 +29,6 @@ const { InteractionEncoder } = encoder;
 const { ResolveInteraction } = interactions;
 
 const Wrapper = styled.div``;
-
-const StyledModal = styled(Modal)`
-  max-width: 400px !important;
-  border-radius: 0 !important;
-`;
-
-const StyledCard = styled.div`
-  margin: 0 !important;
-  padding: 24px !important;
-  position: relative !important;
-  width: 100% !important;
-`;
-
-const CloseBar = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-
-  > svg path {
-    fill: #9da9bb;
-  }
-
-  cursor: pointer;
-`;
-
-const ActionBar = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
-  margin-top: 28px;
-`;
 
 const StyledTitle = styled.header`
   ${p_20_semibold};
@@ -147,34 +117,20 @@ export default function ResolveModal({ open, setOpen, reward, topicCid }) {
     }
   };
 
-  const closeModal = () => setOpen(false);
-
-  const closeButton = (
-    <img onClick={closeModal} src="/imgs/icons/close.svg" width={24} alt="" />
-  );
-
   return (
     <Wrapper>
-      <StyledModal open={open} dimmer onClose={closeModal} size="tiny">
-        <StyledCard>
-          <CloseBar>{closeButton}</CloseBar>
-          <StyledTitle>Resolve</StyledTitle>
+      <Modal open={open} setOpen={setOpen} okText="Confirm" onOk={doConfirm}>
+        <StyledTitle>Resolve</StyledTitle>
 
-          {precent !== 100 && (
-            <Info>If promise is not kept, credit score will be affected.</Info>
-          )}
+        {precent !== 100 && (
+          <Info>If promise is not kept, credit score will be affected.</Info>
+        )}
 
-          <ItemTitle>
-            <StyledText>My promise</StyledText>
-          </ItemTitle>
-          {reward && <PromiseItem reward={reward} />}
-          <ActionBar>
-            <Button primary onClick={doConfirm}>
-              Confirm
-            </Button>
-          </ActionBar>
-        </StyledCard>
-      </StyledModal>
+        <ItemTitle>
+          <StyledText>My promise</StyledText>
+        </ItemTitle>
+        {reward && <PromiseItem reward={reward} />}
+      </Modal>
     </Wrapper>
   );
 }
