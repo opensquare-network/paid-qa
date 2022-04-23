8333b7c15474d665f13a27ba293348edf7a7c9f7
Yongfeng LI <wliyongfeng@gmail.com>
2022-04-23 20:59:15 +0800

    Fix erronous import

--- packages/common/src/utils/index.js
+++ packages/common/src/utils/index.js
@@ -2,6 +2,5 @@ export * from "./address";
 export * from "./cookie";
 export * from "./extension";
 export * from "./hooks";
-export * from "./markdown";
 export * from "./timeDuration";
 export * from "./tokenValue";
