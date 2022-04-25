377b1b6b219cfaf31618772e2b1fd33540d8c0cf
2nthony <ant.hu@outlook.com>
2022-04-25 14:24:28 +0800

    feat(ui): tag
    impl

--- packages/ui/lib/Tag/index.js
+++ packages/ui/lib/Tag/index.js
@@ -1,27 +1,30 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { p_12_medium } from "../styles/textStyles";
+
+const colors = {
+  active: "#6848FF",
+  resolved: "#E2E8F0",
+};
 
 const Wrapper = styled.span`
   padding: 2px 12px;
-  text-transform: capitalize;
   color: white;
-  line-height: 18px;
+  line-height: 16px;
+  font-size: 12px;
+  ${p_12_medium}
+  background-color: ${(p) => colors[p.status] || p.color || "#000"};
+  ${(p) =>
+    p.status &&
+    css`
+      text-transform: capitalize;
+    `}
 `;
 
-const getColor = (tag) => {
-  switch (tag) {
-    case "active":
-      return "#6848FF";
-    default:
-      return "#E2E8F0";
-  }
-};
-
-export default function Tag({ children }) {
+export default function Tag({ children, status = "", color }) {
   return (
-    <Wrapper style={{ backgroundColor: getColor(children) }}>
-      {" "}
-      {children}
+    <Wrapper status={status} color={color}>
+      {status || children}
     </Wrapper>
   );
 }
