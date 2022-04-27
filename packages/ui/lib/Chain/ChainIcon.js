6b8c3463b12333353824575e698822f98e651151
yoshiyuki <brother-zhou@qq.com>
2022-04-27 15:31:54 +0800

    common-ui: complete <ChainIcon> & story

--- packages/ui/lib/Chain/ChainIcon.js
+++ packages/ui/lib/Chain/ChainIcon.js
@@ -7,8 +7,20 @@ import { ReactComponent as Kintsugi } from "../imgs/icons/chain/kintsugi.svg";
 import { ReactComponent as Default } from "../imgs/icons/chain/default.svg";
 import { ReactComponent as Moonriver } from "../imgs/icons/chain/moonriver.svg";
 import { ReactComponent as Westend } from "../imgs/icons/chain/westend.svg";
+import { ReactComponent as Interlay } from "../imgs/icons/chain/interlay.svg";
+import { ReactComponent as Acala } from "../imgs/icons/chain/acala.svg";
+import { ReactComponent as Polkadex } from "../imgs/icons/chain/polkadex.svg";
+import { ReactComponent as Crust } from "../imgs/icons/chain/crust.svg";
+import { ReactComponent as Ethereum } from "../imgs/icons/chain/ethereum.svg";
+import styled from "styled-components";
+import Tooltip from "../Tooltip";
 
-function ChainIcon({ chainName, size = 24 }) {
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+`;
+
+function ResolveChainIcon({ chainName, size = 24 }) {
   switch (chainName) {
     case "polkadot":
       return <Polkadot viewBox="0 0 24 24" width={size} height={size} />;
@@ -28,9 +40,46 @@ function ChainIcon({ chainName, size = 24 }) {
       return <Moonriver viewBox="0 0 24 24" width={size} height={size} />;
     case "westend":
       return <Westend viewBox="0 0 24 24" width={size} height={size} />;
+    case "interlay":
+      return <Interlay viewBox="0 0 24 24" width={size} height={size} />;
+    case "acala":
+      return <Acala viewBox="0 0 24 24" width={size} height={size} />;
+    case "polkadex":
+      return <Polkadex viewBox="0 0 24 24" width={size} height={size} />;
+    case "crust":
+      return <Crust viewBox="0 0 24 24" width={size} height={size} />;
+    case "ethereum":
+      return <Ethereum viewBox="0 0 24 24" width={size} height={size} />;
     default:
       return <Default viewBox="0 0 24 24" width={size} height={size} />;
   }
+}
+
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+
+function ChainIcon({
+  chainName,
+  position,
+  offset,
+  showTooltip = false,
+  size = 24,
+}) {
+  const Icon = ResolveChainIcon({ chainName, size });
+  return (
+    <Wrapper>
+      {Icon}
+      {showTooltip && (
+        <Tooltip
+          content={capitalize(chainName)}
+          position={position}
+          offset={offset}
+          size="full"
+        >
+          <></>
+        </Tooltip>
+      )}
+    </Wrapper>
+  );
 }
 
 export default ChainIcon;
