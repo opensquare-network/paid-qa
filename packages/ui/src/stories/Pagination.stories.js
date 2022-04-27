d71e6d9edddec917d1ede8370a058dd7219701ac
2nthony <ant.hu@outlook.com>
2022-04-27 14:52:54 +0800

    docs(story): pagination update

--- packages/ui/src/stories/Pagination.stories.js
+++ packages/ui/src/stories/Pagination.stories.js
@@ -1,5 +1,6 @@
 import Pagination from "../../lib/styled/Pagination";
 import { useState } from "react";
+import { Link, BrowserRouter } from "react-router-dom";
 
 export default {
   title: "Pagination",
@@ -9,5 +10,60 @@ export default {
 export const primary = () => {
   const [page, setPage] = useState(1);
 
-  return <Pagination page={page} pageSize={10} total="128" setPage={setPage} />;
+  return <Pagination page={page} pageSize={10} total={128} setPage={setPage} />;
+};
+export const large = () => {
+  const [page, setPage] = useState(1);
+
+  return (
+    <Pagination page={page} pageSize={10} total={50} setPage={setPage} large />
+  );
+};
+export const itemRender = () => {
+  const [page, setPage] = useState(1);
+
+  const itemRender = (page, type, originalElement) => {
+    if (type === "page") {
+      return <a>{page}</a>;
+    }
+
+    return originalElement;
+  };
+
+  return (
+    <Pagination
+      page={page}
+      pageSize={10}
+      total={128}
+      setPage={setPage}
+      itemRender={itemRender}
+    />
+  );
+};
+export const routerLink = () => {
+  const [page, setPage] = useState(1);
+
+  const itemRender = (page, type, originalElement) => {
+    if (type === "page") {
+      return <Link to={page}>{page}</Link>;
+    }
+
+    if (type === "prev" || type === "next") {
+      return <Link to={page}>{originalElement}</Link>;
+    }
+
+    return originalElement;
+  };
+
+  return (
+    <BrowserRouter>
+      <Pagination
+        page={page}
+        pageSize={10}
+        total={128}
+        setPage={setPage}
+        itemRender={itemRender}
+      />
+    </BrowserRouter>
+  );
 };
