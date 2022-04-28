b6cdb9b9fc1a3597fac2ad0c3502d9d9881b5ca0
yoshiyuki <brother-zhou@qq.com>
2022-04-28 15:54:13 +0800

    @osn-common-ui: complete export statement & release 0.0.45 (#493)
    * @osn-common-ui: complete export statement & release 0.0.45
    
    * clean: remove redundant styled/index import

--- packages/ui/lib/index.js
+++ packages/ui/lib/index.js
@@ -1,6 +1,5 @@
 import Footer from "./Footer";
 import ExternalLink from "./ExternalLink";
-import Dropdown from "./styled/Dropdown";
 import ChainIcon from "./Chain/ChainIcon";
 import ChainSelectItem from "./Chain/ChainSelectItem";
 import ChainSelector from "./Chain/ChainSelector";
@@ -9,15 +8,31 @@ import Avatar from "./Account/Avatar";
 import MarkdownEditor from "./Editor/MarkdownEditor";
 import IpfsSquare from "./IpfsSquare";
 import Breadcrumb from "./Navi/Breadcrumb";
-import styled from "./styled";
 import Tag from "./Tag";
 import RichEditor from "./RichEdit";
+import NoData from "./NoData";
+//styled
+import Button from "./styled/Button";
+import Card from "./styled/Card";
+import Container from "./styled/Container";
+import DividerWrapper from "./styled/DividerWrapper";
+import Dropdown from "./styled/Dropdown";
+import Flex from "./styled/Flex";
+import FlexBetween from "./styled/FlexBetween";
+import FlexCenter from "./styled/FlexCenter";
+import Input from "./styled/Input";
+import Layout from "./styled/Layout";
+import Main from "./styled/Main";
+import MobileInvisible from "./styled/MobileInvisible";
+import MobileVisible from "./styled/MobileVisible";
+import Pagination from "./styled/Pagination";
+import ProgressBar from "./styled/ProgressBar";
+import SubTitle from "./styled/SubTitle";
 
 export {
   Footer,
   ExternalLink,
   ChainIcon,
-  Dropdown,
   ChainSelectItem,
   ChainSelector,
   Address,
@@ -25,7 +40,23 @@ export {
   MarkdownEditor,
   IpfsSquare,
   Breadcrumb,
-  styled,
   Tag,
   RichEditor,
+  NoData,
+  Button,
+  Card,
+  Container,
+  DividerWrapper,
+  Dropdown,
+  Flex,
+  FlexBetween,
+  FlexCenter,
+  Input,
+  Layout,
+  Main,
+  MobileInvisible,
+  MobileVisible,
+  Pagination,
+  ProgressBar,
+  SubTitle,
 };
