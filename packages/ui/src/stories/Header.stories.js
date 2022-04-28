2776d9a2e53d5e84596a6ea6f442d8a0a10c9c49
2nthony <ant.hu@outlook.com>
2022-04-28 14:23:55 +0800

    fix(ui): header universal (#489)
    * fix(ui): header universal
    
    * docs(story): header update
    
    * refactor(site): header

--- packages/ui/src/stories/Header.stories.js
+++ packages/ui/src/stories/Header.stories.js
@@ -1,17 +1,12 @@
 import Header from "../../lib/Header";
 import styled from "styled-components";
-import { BrowserRouter } from "react-router-dom";
 
 export default {
   title: "Header",
   component: Header,
 };
 
-export const primary = () => (
-  <BrowserRouter>
-    <Header />
-  </BrowserRouter>
-);
+export const primary = () => <Header />;
 export const content = () => {
   const Content = styled.div`
     width: 100%;
@@ -20,13 +15,22 @@ export const content = () => {
   `;
 
   return (
-    <BrowserRouter>
-      <Header>
-        <Content>
-          <div>left content</div>
-          <div>right content</div>
-        </Content>
-      </Header>
-    </BrowserRouter>
+    <Header>
+      <Content>
+        <div>left content</div>
+        <div>right content</div>
+      </Content>
+    </Header>
   );
+};
+export const logoRender = () => {
+  const LogoWrapper = styled.a`
+    cursor: pointer;
+  `;
+
+  const logoRender = (logo) => {
+    return <LogoWrapper href="/">{logo}</LogoWrapper>;
+  };
+
+  return <Header logoRender={logoRender} />;
 };
