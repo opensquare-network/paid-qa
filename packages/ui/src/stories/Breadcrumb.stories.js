8efe6913c948cac5015695b894af79ef112bcb0f
2nthony <ant.hu@outlook.com>
2022-05-05 19:46:34 +0800

    docs(story): update

--- packages/ui/src/stories/Breadcrumb.stories.js
+++ packages/ui/src/stories/Breadcrumb.stories.js
@@ -12,7 +12,29 @@ export const primary = () => {
         <a href="/">Home</a>
       </Breadcrumb.Item>
       <Breadcrumb.Item>Page</Breadcrumb.Item>
-      <Breadcrumb.Item>App</Breadcrumb.Item>
+      <Breadcrumb.Item>Topic</Breadcrumb.Item>
+    </Breadcrumb>
+  );
+};
+export const backButtonRender = () => {
+  const backButtonRender = (button) => {
+    return <a href="/">{button}</a>;
+  };
+
+  return (
+    <Breadcrumb backButtonRender={backButtonRender}>
+      <Breadcrumb.Item>Home</Breadcrumb.Item>
+      <Breadcrumb.Item>Page</Breadcrumb.Item>
+      <Breadcrumb.Item>Topic</Breadcrumb.Item>
+    </Breadcrumb>
+  );
+};
+export const separator = () => {
+  return (
+    <Breadcrumb separator=">">
+      <Breadcrumb.Item>Home</Breadcrumb.Item>
+      <Breadcrumb.Item>Page</Breadcrumb.Item>
+      <Breadcrumb.Item>Topic</Breadcrumb.Item>
     </Breadcrumb>
   );
 };
