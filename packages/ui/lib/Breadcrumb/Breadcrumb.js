a33cfecaebffc9160ec48e2bb42ce20fc2b6b55d
2nthony <ant.hu@outlook.com>
2022-05-06 15:37:17 +0800

    refactor: use colors

--- packages/ui/lib/Breadcrumb/Breadcrumb.js
+++ packages/ui/lib/Breadcrumb/Breadcrumb.js
@@ -2,7 +2,11 @@ import React, { cloneElement } from "react";
 import styled from "styled-components";
 import BreadcrumbItem from "./BreadcrumbItem";
 import { ReactComponent as CaretLeft } from "../imgs/icons/caret-left.svg";
-import { text_dark_major } from "../styles/colors";
+import {
+  netural_grey_200,
+  netural_grey_300,
+  text_dark_major,
+} from "../styles/colors";
 import { p_16_semibold } from "../styles/textStyles";
 
 const Wrapper = styled.nav`
@@ -20,14 +24,14 @@ const BackButton = styled.span`
   align-items: center;
   justify-content: center;
   background-color: #ffffff;
-  border: 1px solid #f0f3f8;
+  border: 1px solid ${netural_grey_200};
   border-radius: 18px;
   cursor: pointer;
   box-shadow: 0px 4px 31px rgba(26, 33, 44, 0.04),
     0px 0.751293px 3.88168px rgba(26, 33, 44, 0.03);
 
   &:hover {
-    border: 1px solid #e2e8f0;
+    border: 1px solid ${netural_grey_300};
     box-shadow: 0px 4px 31px rgba(26, 33, 44, 0.06),
       0px 0.751293px 8px rgba(26, 33, 44, 0.04);
   }
