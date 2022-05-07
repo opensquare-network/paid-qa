255bdd500dff79cde599e0e967150940a24fe8e7
2nthony <ant.hu@outlook.com>
2022-05-07 22:33:58 +0800

    feat: only notification left

--- packages/site/src/components/Notification/Tabs.js
+++ packages/site/src/components/Notification/Tabs.js
@@ -1,10 +1,6 @@
 import Tabs from "@osn/common-ui/lib/Tabs";
 
 export default function NotificationTabs({ value, setValue }) {
-  const items = [
-    { value: "notifications" },
-    { value: "discussions" },
-    { value: "rewards" },
-  ];
+  const items = [{ value: "notifications" }];
   return <Tabs {...{ items, value, setValue }} />;
 }
