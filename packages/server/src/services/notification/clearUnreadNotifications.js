113ee95433071b3413d4ae9a14470dac37b7f206
chaojun <chaojun@opensquare.network>
2022-05-10 16:21:08 +0800

    Update api to allow specify which items to be marked as read

--- packages/server/src/services/notification/clearUnreadNotifications.js
+++ packages/server/src/services/notification/clearUnreadNotifications.js
@@ -1,12 +1,17 @@
 const { Notification } = require("../../models");
 const { toPublicKey } = require("../../utils/address");
 
-async function clearUnreadNotifications(network, address) {
+async function clearUnreadNotifications(network, address, items) {
   const publicKey = toPublicKey(address);
   const q = {
     owner: publicKey,
     read: false,
   };
+
+  if (Array.isArray(items)) {
+    q._id = { $in: items };
+  }
+
   const result = await Notification.updateMany(q, { read: true });
   return {
     result,
