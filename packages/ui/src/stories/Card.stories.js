90033f39d4ab3427778ef76a8aa4a3f2dba41426
2nthony <ant.hu@outlook.com>
2022-05-11 12:34:18 +0800

    docs(story): card (#533)
    * fix(ui): safe card classname with osn prefix
    
    * docs(story): card

--- packages/ui/src/stories/Card.stories.js
+++ packages/ui/src/stories/Card.stories.js
@@ -1,4 +1,4 @@
-import Card from "../../lib/styled/Card";
+import Card from "../../lib/Card";
 import styled from "styled-components";
 
 export default {
@@ -6,10 +6,87 @@ export default {
   component: Card,
 };
 
-const StyledCard = styled(Card)`
-  background-color: #fee2e2;
-  color: #ef4444;
+const DemoWrapper = styled.div`
+  .osn-card {
+    margin-bottom: 30px;
+  }
 `;
 
-export const primary = () => <Card>Card content</Card>;
-export const styledCard = () => <StyledCard>Card content</StyledCard>;
+const Link = styled.a`
+  color: #40a9ff;
+  cursor: pointer;
+  &:hover {
+    color: #096dd9;
+  }
+`;
+
+export const primary = () => (
+  <DemoWrapper>
+    <Card title="Default size card" extra={<Link href="/">Extra link</Link>}>
+      <p>Card content</p>
+    </Card>
+    <Card
+      title="Small size card"
+      size="small"
+      extra={<Link href="/">Extra link</Link>}
+    >
+      <p>Card content</p>
+    </Card>
+  </DemoWrapper>
+);
+export const noShadow = () => (
+  <DemoWrapper>
+    <Card
+      title="Default size card"
+      shadow={false}
+      extra={<Link href="/">Extra link</Link>}
+    >
+      <p>Card content</p>
+    </Card>
+    <Card
+      title="Small size card"
+      size="small"
+      shadow={false}
+      extra={<Link href="/">Extra link</Link>}
+    >
+      <p>Card content</p>
+    </Card>
+  </DemoWrapper>
+);
+export const noBorder = () => (
+  <DemoWrapper>
+    <Card
+      title="Default size card"
+      bordered={false}
+      extra={<Link href="/">Extra link</Link>}
+    >
+      <p>Card content</p>
+    </Card>
+    <Card
+      title="Small size card"
+      size="small"
+      bordered={false}
+      extra={<Link href="/">Extra link</Link>}
+    >
+      <p>Card content</p>
+    </Card>
+  </DemoWrapper>
+);
+export const onlyContent = () => (
+  <Card>
+    <p>Card content</p>
+  </Card>
+);
+export const customizeHead = () => {
+  const Heading = styled.h1`
+    font-weight: lighter;
+    color: #333;
+    font-size: 3rem;
+  `;
+
+  return (
+    <Card head={<Heading>Customized heading</Heading>}>
+      <p>Card content</p>
+    </Card>
+  );
+};
