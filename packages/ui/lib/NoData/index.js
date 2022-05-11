3e0551a3761f8da1c60b0c4d34c40267ffc8e2fd
2nthony <ant.hu@outlook.com>
2022-05-11 14:06:23 +0800

    refactor(ui): no data

--- packages/ui/lib/NoData/index.js
+++ packages/ui/lib/NoData/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import FlexCenter from "../styled/FlexCenter";
-import { p_14_normal } from "../styles/textStyles";
+import Card from "../Card";
+import { text_dark_accessory } from "../styles/colors";
 
-const Wrapper = styled(FlexCenter)`
-  background: #ffffff;
-  height: 131px;
-  ${p_14_normal};
-  color: #9da9bb;
-  border: 1px solid #f0f3f8;
-  box-shadow: 0px 4px 31px rgba(26, 33, 44, 0.04),
-    0px 0.751293px 3.88168px rgba(26, 33, 44, 0.03);
+const Message = styled(FlexCenter)`
+  color: ${text_dark_accessory};
+  height: 100%;
 `;
 
 export default function NoData({ message = "No current data" }) {
-  return <Wrapper>{message}</Wrapper>;
+  return (
+    <Card style={{ height: 130 }}>
+      <Message>{message}</Message>
+    </Card>
+  );
 }
