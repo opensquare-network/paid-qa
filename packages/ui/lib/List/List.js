7a9ff1fc34427f0994a78612adb2d3698c4251d5
2nthony <ant.hu@outlook.com>
2022-05-11 16:24:06 +0800

    feat(ui): list no data support

--- packages/ui/lib/List/List.js
+++ packages/ui/lib/List/List.js
@@ -1,14 +1,21 @@
-// not ready for public
 import React from "react";
 import styled from "styled-components";
 import ListItem from "./ListItem";
+import NoData from "../NoData";
 
 const ListWrapper = styled.ul`
   padding: 0;
 `;
 
 function List(props) {
-  const { children, data = [], gap, itemRender, ...restProps } = props;
+  const {
+    children,
+    data = [],
+    gap,
+    itemRender,
+    noDataMessage,
+    ...restProps
+  } = props;
 
   if (typeof itemRender !== "function") {
     return null;
@@ -16,8 +23,12 @@ function List(props) {
 
   return (
     <ListWrapper {...restProps}>
-      {data?.map((item, index) =>
-        React.cloneElement(itemRender(item, index), { key: index, gap })
+      {data?.length ? (
+        data?.map((item, index) =>
+          React.cloneElement(itemRender(item, index), { key: index, gap })
+        )
+      ) : (
+        <NoData message={noDataMessage} />
       )}
     </ListWrapper>
   );
