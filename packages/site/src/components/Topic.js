8ed1bbeecfeaa9493c1b18b32f185cc54b4137a8
2nthony <ant.hu@outlook.com>
2022-05-12 12:19:30 +0800

    refactor(site): topic component
    use card

--- packages/site/src/components/Topic.js
+++ packages/site/src/components/Topic.js
@@ -5,37 +5,20 @@ import TagList from "./TagList";
 import DividerWrapper from "@osn/common-ui/lib/styled/DividerWrapper";
 import MobileInvisible from "@osn/common-ui/lib/styled/MobileInvisible";
 import MobileVisible from "@osn/common-ui/lib/styled/MobileVisible";
-import Time from "@osn/common-ui/lib/Time";
 import NetworkUser from "./User/NetworkUser";
 import { encodeNetworkAddress } from "@osn/common/src/utils/address";
-import { MOBILE_SIZE } from "@osn/consts";
 import {
   p_14_normal,
   p_16_semibold,
   p_20_semibold,
 } from "@osn/common-ui/lib/styles/textStyles";
-import FlexCenter from "@osn/common-ui/lib/styled/FlexCenter";
-
-const Wrapper = styled.div`
-  padding: 24px;
-  @media screen and (max-width: ${MOBILE_SIZE}px) {
-    padding: 16px;
-  }
-  display: flex;
-  background: #ffffff;
-  border: 1px solid #f0f3f8;
-  box-shadow: 0px 4px 31px rgba(26, 33, 44, 0.04),
-    0px 0.751293px 3.88168px rgba(26, 33, 44, 0.03);
-  color: #1e2134;
-  margin-bottom: 20px;
-`;
+import { Card, Time, FlexCenter } from "@osn/common-ui";
 
 const ReplyWrapper = styled(FlexCenter)`
   width: 80px;
   height: 80px;
   flex-direction: column;
   border: 1px solid #f0f3f8;
-  margin-right: 24px;
   flex: 0 0 auto;
   > :first-child {
     ${p_20_semibold};
@@ -58,12 +41,10 @@ const DetailWrapper = styled.div`
 const Title = styled(Link)`
   ${p_16_semibold};
   cursor: pointer;
-`;
 
-const Divider = styled.div`
-  height: 1px;
-  margin: 16px 0;
-  background: #f0f3f8;
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 const RepliesCount = styled.div`
@@ -71,36 +52,40 @@ const RepliesCount = styled.div`
 `;
 
 export default function Topic({ topic }) {
-  const signerAddress = encodeNetworkAddress(topic.signer, topic.network);
+  const signerAddress = encodeNetworkAddress(topic?.signer, topic?.network);
+
   return (
-    <Wrapper>
-      <MobileInvisible>
-        <ReplyWrapper>
-          <div>{topic.answersCount || 0}</div>
-          <div>Replies</div>
-        </ReplyWrapper>
-      </MobileInvisible>
+    <Card
+      size="small"
+      prefix={
+        <MobileInvisible>
+          <ReplyWrapper>
+            <div>{topic?.answersCount || 0}</div>
+            <div>Replies</div>
+          </ReplyWrapper>
+        </MobileInvisible>
+      }
+      head={<Title to={`/topic/${topic?.cid}`}>{topic?.title}</Title>}
+    >
       <DetailWrapper>
-        <Title to={`/topic/${topic.cid}`}>{topic.title}</Title>
-        <Divider />
         <div>
           <DividerWrapper>
             <MobileInvisible>
               <NetworkUser
                 address={signerAddress}
-                network={topic.network}
+                network={topic?.network}
                 iconSize={14}
                 tooltipPosition="down"
               />
             </MobileInvisible>
             <MobileVisible>
-              <RepliesCount>{topic.answersCount || 0} Replies</RepliesCount>
+              <RepliesCount>{topic?.answersCount || 0} Replies</RepliesCount>
             </MobileVisible>
-            <Time time={topic.indexer.blockTime} />
+            <Time time={topic?.indexer?.blockTime} />
           </DividerWrapper>
           <TagList topic={topic} />
         </div>
       </DetailWrapper>
-    </Wrapper>
+    </Card>
   );
 }
