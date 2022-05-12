8b90eda0ec2221aa22db8299d88c5a191be2b87f
2nthony <ant.hu@outlook.com>
2022-05-12 17:46:52 +0800

    fix(ui): time
    move utils

--- packages/ui/lib/Time/index.js
+++ packages/ui/lib/Time/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { timeDuration } from "@osn/common";
+import { timeDuration } from "./utils";
 
 const Wrapper = styled.div`
   font-size: 14px;
