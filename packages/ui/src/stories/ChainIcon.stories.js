ed5acda3f9c16af2ba49dfe2a2be7e2442f02116
yoshiyuki <brother-zhou@qq.com>
2022-05-13 14:37:41 +0800

    complete <ChainIcon>

--- packages/ui/src/stories/ChainIcon.stories.js
+++ packages/ui/src/stories/ChainIcon.stories.js
@@ -11,18 +11,24 @@ export const Unknown = () => <ChainIcon />;
 export const Available = () => {
   return (
     <Flex style={{ gap: 8, padding: 40 }}>
-      <ChainIcon chainName="polkadot" showTooltip />
+      <ChainIcon chainName="default" showTooltip />
       <ChainIcon chainName="kusama" showTooltip />
       <ChainIcon chainName="statemine" showTooltip />
+      <ChainIcon chainName="polkadot" showTooltip />
+      <ChainIcon chainName="statemint" showTooltip />
+      <ChainIcon chainName="westend" showTooltip />
+      <ChainIcon chainName="westmint" showTooltip />
       <ChainIcon chainName="karura" showTooltip />
       <ChainIcon chainName="khala" showTooltip />
+      <ChainIcon chainName="moonriver" showTooltip />
+      <ChainIcon chainName="moonbeam" showTooltip />
       <ChainIcon chainName="bifrost" showTooltip />
       <ChainIcon chainName="kintsugi" showTooltip />
-      <ChainIcon chainName="moonriver" showTooltip />
-      <ChainIcon chainName="westend" showTooltip />
-      <ChainIcon chainName="interlay" showTooltip />
-      <ChainIcon chainName="acala" showTooltip />
       <ChainIcon chainName="polkadex" showTooltip />
+      <ChainIcon chainName="centrifuge" showTooltip />
+      <ChainIcon chainName="interlay" showTooltip />
+      <ChainIcon chainName="chrwna" showTooltip />
+      <ChainIcon chainName="acala" showTooltip />
       <ChainIcon chainName="crust" showTooltip />
       <ChainIcon chainName="ethereum" showTooltip />
     </Flex>
