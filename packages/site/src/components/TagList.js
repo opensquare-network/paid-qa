64a497e5b19c9dc08fa69b0eedd838f35c9314d8
yoshiyuki <brother-zhou@qq.com>
2022-05-16 17:47:18 +0800

    show the promised fund when created only, #573

--- packages/site/src/components/TagList.js
+++ packages/site/src/components/TagList.js
@@ -29,7 +29,7 @@ export default function TagList({ topic }) {
     return null;
   }
   const tokenValues = calcRewards(topic.rewards);
-
+  const symbol = topic.rewards[0].bounty.symbol;
   return (
     <Wrapper>
       {
@@ -37,12 +37,10 @@ export default function TagList({ topic }) {
         topic.status === "resolved" ? (
           <StatusTag status={topic.status} />
         ) : (
-          Object.keys(tokenValues).map((symbol) => (
-            <Tag
-              key={symbol}
-              color={getSymbolColor(symbol)}
-            >{`${tokenValues[symbol]} ${symbol}`}</Tag>
-          ))
+          <Tag
+            key={symbol}
+            color={getSymbolColor(symbol)}
+          >{`${tokenValues[symbol]} ${symbol}`}</Tag>
         )
       }
     </Wrapper>
