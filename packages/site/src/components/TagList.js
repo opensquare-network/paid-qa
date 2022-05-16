b192550eebf5f198d5d92690de351088bebbabae
2nthony <ant.hu@outlook.com>
2022-05-16 16:26:30 +0800

    feat(site): colorful tag (#589)

--- packages/site/src/components/TagList.js
+++ packages/site/src/components/TagList.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { calcRewards } from "utils/rewards";
 import { Tag } from "@osn/common-ui";
 import StatusTag from "./StatusTag";
+import { primary_purple_500 } from "@osn/common-ui/lib/styles/colors";
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,19 +11,13 @@ const Wrapper = styled.div`
   }
 `;
 
-function getSymbolColor(symbol) {
-  switch (symbol) {
-    case "KSM": {
-      return "#000000";
-    }
-    case "DOT": {
-      return "#E6007A";
-    }
-    default: {
-      return "#000000";
-    }
-  }
-}
+const symbolColorMap = {
+  KSM: "#000000",
+  DOT: "#E6007A",
+  OSNT: primary_purple_500,
+  WND: "linear-gradient(90.04deg, rgba(247, 148, 32, 0.92) 2.41%, rgba(196, 196, 196, 0) 164.5%), #E6007A",
+};
+const getSymbolColor = (symbol) => symbolColorMap[symbol] || "#000000";
 
 export default function TagList({ topic }) {
   if (!topic) {
