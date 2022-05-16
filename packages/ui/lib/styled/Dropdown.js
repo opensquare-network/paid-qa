d8a523d87fb45dd9e0bd92e620e8a3015c744156
2nthony <ant.hu@outlook.com>
2022-05-16 09:50:44 +0800

    feat(ui): dropdown improvement

--- packages/ui/lib/styled/Dropdown.js
+++ packages/ui/lib/styled/Dropdown.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { Dropdown } from "semantic-ui-react";
+import CaretDownIcon from "../imgs/icons/caret-down.svg";
+import { neutral_grey_500 } from "../styles/colors";
 
 const StyledDropdown = styled(Dropdown)`
   width: 100%;
@@ -23,11 +25,44 @@ const StyledDropdown = styled(Dropdown)`
   }
   .icon {
     top: 50% !important;
-    transform: translate(0, -9px) !important;
-    opacity: 0.24 !important;
+    transform: translate(0, -50%) !important;
+    background-image: url(${CaretDownIcon});
+    width: 24px !important;
+    height: 24px !important;
+    right: 16px !important;
+    margin: 0 !important;
+
+    ::before {
+      content: " " !important;
+    }
+  }
+
+  &.active {
+    .icon {
+      transform: translate(0, -50%) rotate(180deg) !important;
+    }
   }
   .menu {
     border-radius: 0 !important;
+
+    .item {
+      border-top: none !important;
+    }
+
+    ::-webkit-scrollbar {
+      width: 10px;
+    }
+    ::-webkit-scrollbar-track {
+      background-color: transparent;
+    }
+    ::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      width: 10px;
+      padding: 0px;
+      background-color: ${neutral_grey_500};
+      border: 2px solid transparent;
+      background-clip: padding-box;
+    }
   }
 `;
 
