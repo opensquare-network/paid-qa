6b65a319d7f68d43b32bf4a769549c6f3f878894
2nthony <ant.hu@outlook.com>
2022-05-23 01:51:07 +0800

    docs(story): toast

---
+++ packages/ui/src/stories/Toast.stories.js
@@ -0,0 +1,70 @@
+import styled from "styled-components";
+import Button from "../../lib/styled/Button";
+import { createToast, destroyAllToast } from "../../lib/Toast";
+
+export default {
+  title: "Toast",
+};
+
+const DemoWrapper = styled.div`
+  button {
+    margin: 0 1rem 1rem 0;
+  }
+`;
+
+export const primary = () => {
+  return (
+    <DemoWrapper>
+      <Button
+        onClick={() => {
+          createToast({
+            title: "Default",
+            message: "Message",
+          });
+        }}
+      >
+        Default
+      </Button>
+      <br />
+      <Button
+        onClick={() => {
+          createToast({
+            title: "Success",
+            message: "Message",
+            type: "success",
+          });
+        }}
+      >
+        Success
+      </Button>
+      <br />
+      <Button
+        onClick={() => {
+          createToast({
+            title: "Pending",
+            message: "Message",
+            type: "pending",
+          });
+        }}
+      >
+        Pending
+      </Button>
+      <br />
+      <Button
+        onClick={() => {
+          createToast({
+            title: "Info",
+            message: "Message",
+            type: "info",
+          });
+        }}
+      >
+        Info
+      </Button>
+      <br />
+      <Button primary onClick={destroyAllToast}>
+        Destroy All
+      </Button>
+    </DemoWrapper>
+  );
+};
