b632da7687c5ed362a917bba6e1bbe5d941ad1b5
2nthony <ant.hu@outlook.com>
2022-05-23 16:05:39 +0800

    fi

--- packages/ui/lib/Toast/ToastItem.js
+++ packages/ui/lib/Toast/ToastItem.js
@@ -37,7 +37,6 @@ const ToastItemWrapper = styled.div`
   ${(p) =>
     p.slideIn &&
     css`
-      /* FIXME: not the expected animation */
       transform: translateX(0)
         ${p.sortedIndex
           ? `translateY(calc(${p.sortedIndex * 100}% + ${
