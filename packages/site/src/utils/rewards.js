81adeeba836ed1d6692dee5a5f9746ecc38a0b07
chaojun <chaojun@opensquare.network>
2022-06-20 16:48:45 +0800

    Update promises to supports

--- packages/site/src/utils/rewards.js
+++ packages/site/src/utils/rewards.js
@@ -15,10 +15,12 @@ export function calcRewards(rewards) {
 export function calcSponserRewards(rewards) {
   //TODO: handle different asset with same symbol name
   const tokenValues = {};
-  rewards?.forEach((reward) => {
-    const key = `${reward.network}:${reward.sponsor}:${reward.bounty.symbol}`;
-    tokenValues[key] = bnAdd(tokenValues[key] ?? "0", reward.bounty.value);
-  });
+  rewards
+    ?.filter((r) => r.type === "support")
+    .forEach((reward) => {
+      const key = `${reward.network}:${reward.sponsor}:${reward.bounty.symbol}`;
+      tokenValues[key] = bnAdd(tokenValues[key] ?? "0", reward.bounty.value);
+    });
   return Object.keys(tokenValues).map((key) => {
     const [network, sponsor, symbol] = key.split(":");
     const value = tokenValues[key];
