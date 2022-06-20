60d78d80adceeda4982b33bbb665097508c0faf4
2nthony <ant.hu@outlook.com>
2022-06-20 17:46:28 +0800

    fix: notification item css

--- packages/site/src/components/Notification/NotificationItem.js
+++ packages/site/src/components/Notification/NotificationItem.js
@@ -57,6 +57,7 @@ const TitleWrapper = styled(Flex)`
 const InfoWrapper = styled(FlexBetween)`
   flex: 1;
   max-width: 50%;
+  display: flex;
 
   @media screen and (max-width: ${MOBILE_SIZE}px) {
     max-width: 100%;
@@ -95,9 +96,18 @@ const Title = styled.p`
   }
 `;
 
+const NetworkUserWrapper = styled(Flex)`
+  flex: 1;
+`;
+const TimeWrapper = styled(Flex)`
+  flex: 1;
+  justify-content: flex-end;
+`;
 const StatusWrapper = styled(Flex)`
+  flex: 1;
   width: 18px;
   height: 18px;
+  justify-content: flex-end;
 
   @media screen and (max-width: ${MOBILE_SIZE}px) {
     display: none;
@@ -108,8 +118,8 @@ const MarkAsReadButton = styled.button`
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  width: 100%;
-  height: 100%;
+  width: 18px;
+  height: 18px;
   padding: 0;
   border: none;
   background-color: transparent;
@@ -211,14 +221,18 @@ export default function NotificationItem({ data, onMarkAsRead = () => {} }) {
             </TitleWrapper>
 
             <InfoWrapper>
-              <NetworkUser
-                address={byWho.address}
-                network={byWho.network}
-                iconSize={16}
-                tooltipPosition="down"
-              />
+              <NetworkUserWrapper>
+                <NetworkUser
+                  address={byWho.address}
+                  network={byWho.network}
+                  iconSize={16}
+                  tooltipPosition="down"
+                />
+              </NetworkUserWrapper>
 
-              <Time time={topic.createdAt} />
+              <TimeWrapper>
+                <Time time={topic.createdAt} />
+              </TimeWrapper>
 
               <StatusWrapper>
                 {!read ? (
