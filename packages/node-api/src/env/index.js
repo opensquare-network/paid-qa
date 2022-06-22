8d65fad934383112e3e5842337171e76c33edd99
Yongfeng LI <wliyongfeng@gmail.com>
2022-06-22 16:33:00 +0800

    Add dependency to @osn/polkadot-api-container (#660)

--- packages/node-api/src/env/index.js
+++ packages/node-api/src/env/index.js
@@ -1,27 +1,18 @@
 const { Chains } = require("../constants");
 
+const chainEndpointsMap = {
+  [Chains.polkadot]: process.env.DOT_ENDPOINTS,
+  [Chains.kusama]: process.env.KSM_ENDPOINTS,
+  [Chains.westend]: process.env.WND_ENDPOINTS,
+  [Chains.statemine]: process.env.STATEMINE_ENDPOINTS,
+  [Chains.statemint]: process.env.STATEMINT_ENDPOINTS,
+  [Chains.westmint]: process.env.WESTMINT_ENDPOINTS,
+};
+
 // [chain, endpoints]
 const endpoints = Object.values(Chains).map((chain) => {
-  let endpoints = [];
-  if (Chains.polkadot === chain) {
-    endpoints = (process.env.DOT_ENDPOINTS || "").split(";");
-  } else if (Chains.kusama === chain) {
-    endpoints = (process.env.KSM_ENDPOINTS || "").split(";");
-  } else if (Chains.karura === chain) {
-    endpoints = (process.env.KAR_ENDPOINTS || "").split(";");
-  } else if (Chains.khala === chain) {
-    endpoints = (process.env.KHA_ENDPOINTS || "").split(";");
-  } else if (Chains.statemine === chain) {
-    endpoints = (process.env.STATEMINE_ENDPOINTS || "").split(";");
-  } else if (Chains.bifrost === chain) {
-    endpoints = (process.env.BNC_ENDPOINTS || "").split(";");
-  } else if (Chains.kintsugi === chain) {
-    endpoints = (process.env.KINT_ENDPOINTS || "").split(";");
-  } else if (Chains.westend === chain) {
-    endpoints = (process.env.WND_ENDPOINTS || "").split(";");
-  } else if (Chains.westmint === chain) {
-    endpoints = (process.env.WESTMINT_ENDPOINTS || "").split(";");
-  }
+  let endpoints;
+  endpoints = (chainEndpointsMap[chain] || "").split(";");
 
   return {
     chain,
