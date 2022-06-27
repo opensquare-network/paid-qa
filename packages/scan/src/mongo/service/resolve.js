6172204c17c06fb2b228dda2b5254fcbb472dee7
Yongfeng LI <wliyongfeng@gmail.com>
2022-06-27 22:12:45 +0800

    Guard same author resolve same topic

--- packages/scan/src/mongo/service/resolve.js
+++ packages/scan/src/mongo/service/resolve.js
@@ -2,13 +2,25 @@ const { Resolve } = require("@paid-qa/backend-common/src/models/scan");
 const { busLogger } = require("@osn/scan-common");
 
 async function insertResolve(resolve) {
-  const maybeInDb = await Resolve.findOne({
+  let maybeInDb = await Resolve.findOne({
     "indexer.blockHash": resolve.indexer.blockHash,
     "indexer.extrinsicIndex": resolve.indexer.extrinsicIndex,
   });
   if (maybeInDb) {
     busLogger.info(
       `Same resolve ${resolve.topicCid} has existed in DB, #${resolve.indexer.blockHeight}`
+    );
+    return;
+  }
+
+  maybeInDb = await Resolve.findOne({
+    topicCid: resolve.topicCid,
+    sponsorPublicKey: resolve.sponsorPublicKey,
+  });
+
+  if (maybeInDb) {
+    busLogger.info(
+      `Same resolver and topic ${resolve.sponsor} ${resolve.topicCid} has existed in DB, #${resolve.indexer.blockHeight}`
     );
     return;
   }
