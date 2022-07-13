f81992b779f0d9045cfa6943dddff6bb66c35347
2nthony <ant.hu@outlook.com>
2022-07-13 11:21:12 +0800

    feat(common): state

---
+++ packages/common/src/state/useAsyncState.js
@@ -0,0 +1,57 @@
+// @ts-check
+import { useState, useEffect } from "react";
+
+/**
+ * @typedef {{
+ * immediate?: boolean
+ * onError?: (e: unknown) => void
+ * }} useAsyncStateOption
+ */
+/**
+ * @copyright vueuse
+ * @param {Promise<any> | ((...args: any[]) => Promise<any>)} promise
+ * @param {any} initialState
+ * @param {useAsyncStateOption} options
+ */
+export function useAsyncState(promise, initialState, options) {
+  const { immediate = true, onError = () => {} } = options ?? {};
+
+  const [state, setState] = useState(initialState);
+  const [error, setError] = useState(null);
+  const [isReady, setIsReady] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const execute = async (...args) => {
+    setIsReady(false);
+    setIsLoading(true);
+
+    const p = typeof promise === "function" ? promise(...args) : promise;
+
+    try {
+      const data = await p;
+
+      setState(data);
+      setIsReady(true);
+    } catch (e) {
+      setError(e);
+      onError(e);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (immediate) {
+      execute();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    state,
+    error,
+    isReady,
+    isLoading,
+    execute,
+  };
+}
