97cc1468a10f3f1455d40203c742e8db949c96ca
2nthony <ant.hu@outlook.com>
2022-07-22 20:00:05 +0800

    collapse

---
+++ packages/ui/lib/Collapse/styled.js
@@ -0,0 +1,48 @@
+import styled, { css } from "styled-components";
+
+export const CollapseWrapper = styled.div`
+  ${(p) =>
+    p.collapsed &&
+    css`
+      .osn-collapse-body {
+        display: none;
+      }
+      .osn-card-body {
+        .osn-divider {
+          display: none;
+        }
+      }
+    `}
+
+  ${(p) =>
+    p.ghost &&
+    css`
+      .osn-card {
+        padding: 0;
+      }
+      .osn-card-body {
+        .osn-divider {
+          display: none;
+        }
+      }
+      .osn-collapse-body {
+        margin-top: 16px;
+      }
+    `}
+`;
+
+export const CollapseButton = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background: none;
+  cursor: pointer;
+
+  ${(p) =>
+    p.collapsed &&
+    css`
+      svg {
+        transform: rotate(180deg);
+      }
+    `}
+`;
