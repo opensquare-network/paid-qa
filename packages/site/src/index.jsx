702e74ccdc7b2123ca14d2b416e6bf245c1d7f5b
2nthony <ant.hu@outlook.com>
2023-05-18 11:51:54 +0800

    chore: deps (#798)
    * react 18
    
    * osn
    
    * lockfile
    
    * redux
    
    * move
    
    * fix semantic ui css to 2.4.1

--- packages/site/src/index.jsx
+++ packages/site/src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 
 import App from "./App";
@@ -8,9 +8,10 @@ import "semantic-ui-css/semantic.min.css";
 import "@osn/common-ui/reset.css";
 import "react-mde/lib/styles/css/react-mde-all.css";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+
+root.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root"),
 );
